4c9dd14a04630ca26fa6dee5ec4c095e84e42ea9
Enzo2601 <nguyenviethoan3333@gmail.com>
2024-01-27 16:34:58 +0800

    update my information

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // Nhập khẩu Express
 const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 // Tạo một ứng dụng Express mới
-const app = express();
+
 
 // Xác định cổng mà server sẽ lắng nghe
 const port = 3000;
 
-// Xác lập một route cơ bản
-app.get('/', (req, res) => {
-  res.send('Xin chào từ Node.js hahahaha !');
+
+app.get('/', function(req, res) {
+  res.render('home');
 });
+
 
 // Bắt đầu lắng nghe trên cổng đã xác định
 app.listen(port, () => {
